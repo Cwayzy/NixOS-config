26ce89362e1989984d222990c683defc568ac19c
Kevin <kevinmannigo7@gmail.com>
2026-07-29 20:16:29 +0300

    refresh

--- hosts/C-PC/C-PC.nix
+++ hosts/C-PC/C-PC.nix
@@ -1,9 +1,24 @@
-{ ... }:
+{ config, lib, pkgs, vars, ... }:
 {
   imports = [
     ./hardware-configuration.nix
-    ../../configuration.nix
+    ./configuration.nix
   ];
 
   networking.hostName = "C-PC";
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEftiVariables = true;
+
+  services.getty.autologinUser = vars.username;
+
+  users.users.${vars.username} = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    packages = with pkgs; [
+      tree
+    ];
+  };
+
+  system.stateVersion = "26.05";
 }
---
+++ hosts/C-PC/configuration.nix
@@ -0,0 +1,68 @@
+{ config, lib, pkgs, stylix, ... }:
+
+{
+  imports =
+    [ 
+      ./modules/system/system.nix
+    ];
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.supportedFilesystems = [ "nfs"];
+
+  networking.networkmanager.enable = true;
+  services.tailscale.enable = true;
+
+  time.timeZone = "Europe/Tallinn";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  console = {
+     font = "Lat2-Terminus16";
+     keyMap = "et";
+   };
+
+  nixpkgs.config.allowUnfree = true;
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  services.pipewire = {
+     enable = true;
+     wireplumber.enable = true;
+     alsa.enable = true;
+     alsa.support32Bit = true;
+     pulse.enable = true;
+  };
+
+   environment.systemPackages = with pkgs; [
+     neovim
+     killall
+     alsa-plugins
+     bluez
+     libnotify
+     font-awesome
+     wget
+     gcc
+   ];
+
+  security.pam.services.hyprlock = {};
+  programs.nix-ld.enable = true; 
+      
+  fonts.packages = with pkgs; [
+	noto-fonts
+	adwaita-fonts
+	nerd-fonts.jetbrains-mono
+	noto-fonts-cjk-sans
+  ];
+  
+  nix.gc = {
+	automatic = true;
+	dates = "weekly";
+	options = "--delete-older-than 7d";
+  };
+
+  nix.settings.auto-optimise-store = true;
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+}
+
