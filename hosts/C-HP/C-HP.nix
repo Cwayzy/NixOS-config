56cf4f4b51d3b0ef3bd3c332226f459e85471700
Kevin Männigo <KevinMannigo7@gmail.com>
2026-07-31 02:21:39 +0300

    getting started

--- hosts/C-HP/C-HP.nix
+++ hosts/C-HP/C-HP.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, lib, pkgs, vars, ... }:
 {
   imports = [
     ./hardware-configuration.nix
@@ -6,4 +6,17 @@
   ];
 
   networking.hostName = "C-HP";
+	
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  users.users.${vars.username} = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    packages = with pkgs; [
+      tree
+    ];
+  };
+
+  system.stateVersion = "26.05";
 }
--- modules/user/shell/shell.nix
+++ modules/user/shell/shell.nix
@@ -13,14 +13,14 @@ in
       shellAliases = {
         n = "nvim";
         sn = "sudo nvim";
-        umod = "cd /home/kevin/.dotfiles/nixos/modules/user";
-        smod = "cd /home/kevin/.dotfiles/nixos/modules/system";
+        umod = "cd /home/kevin/.dotfiles/modules/user";
+        smod = "cd /home/kevin/.dotfiles/modules/system";
         zen-browser = "zen-beta";
         conf = "cd $HOME/.config/hypr";
-        nconf = "cd /home/kevin/.dotfiles/nixos/";
-        update = "sudo nixos-rebuild switch --flake $HOME/.dotfiles/nixos#C-PC";
-        hupdate = "home-manager switch --flake $HOME/.dotfiles/nixos#kevin";
-        upgrade = "cd $HOME/.dotfiles/nixos && nix flake update && sudo nixos-rebuild switch --flake $HOME/.dotfiles/nixos#C-PC";
+        nconf = "cd /home/kevin/.dotfiles/";
+        update = "sudo nixos-rebuild switch --flake $HOME/.dotfiles#C-HP";
+        hupdate = "home-manager switch --flake $HOME/.dotfiles#kevin";
+        upgrade = "cd $HOME/.dotfiles/ && nix flake update && sudo nixos-rebuild switch --flake $HOME/.dotfiles#C-PC";
       };
       profileExtra = ''
         if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
