ffaec9f4f1af9d484e8c8a0b78a3bf278bc7ddc8
Kevin <kevinmannigo7@gmail.com>
2026-07-30 13:23:35 +0300

    fixes

--- modules/system/system.nix
+++ modules/system/system.nix
@@ -1,16 +1,16 @@
 { ... }:
 {
   imports = [
-    ./sddm/sddm.nix
+    #./sddm/sddm.nix
     ./mount/mount.nix
     ./bluetooth/bluetooth.nix
   ];
 
   modules.system = {
-    sddm = {
-      enable = true;
-      autologin = true;
-    };
+    #sddm = {
+    #  enable = true;
+    #  autologin = true;
+    #};
 
     mount.enable = true;
     bluetooth.enable = true;
