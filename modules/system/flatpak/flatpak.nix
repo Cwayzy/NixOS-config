696ef1ed557c4e23377ab10ae6cfc26299e910d9
Kevin Männigo <KevinMannigo7@gmail.com>
2026-08-01 18:15:49 +0300

    flatpak

---
+++ modules/system/flatpak/flatpak.nix
@@ -0,0 +1,31 @@
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.modules.system.flatpak;
+in
+{
+  imports = [
+    inputs.nix-flatpak.nixosModules.nix-flatpak
+  ];
+
+  options.modules.system.flatpak = {
+    enable = lib.mkEnableOption "Enable flatpak";
+
+    packages = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [];
+      description = "List of Flatpak application IDs to install";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.flatpak = {
+      enable = true;
+      remotes = [{
+        name = "flathub";
+        location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+      }];
+      packages = cfg.packages;
+      update.onActivation = true;
+    };
+  };
+}
